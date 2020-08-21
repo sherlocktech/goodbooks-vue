a164211e06df4f8e7f9d2960fc0f04fbbdc2343c
Sean <sean.stamm5@gmail.com>
2020-08-21 13:35:03 -0700

    Add Book component

---
+++ src/components/Book.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="book-wrapper">
+    <div class="book-title">
+      {{ book.title }}
+    </div>
+    <div class="book-author">
+      {{ book.author }}
+    </div>
+    <div class="book-created">
+      {{ book.createdOn }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import IBook from '@/types/Book';
+
+@Component({
+  name: "Book",
+  components: {}
+})
+export default class Book extends Vue {
+  @Prop({ required: true })
+  book!: IBook;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.book-wrapper {
+  margin: 0.8rem;
+  padding: 0.4rem;
+  border: 1px solid #555;
+  border-radius: 1rem;
+}
+
+.book-title {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+.book-author {
+  color: #333;
+}
+
+.book-created {
+  font-size: 0.9rem;
+  color: #333;
+}
+</style>
--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,41 +1,34 @@
 <template>
-  <div class="about">
+  <div class="books-container">
     <h1>My Books</h1>
     <div v-if="myBooks.length">
-      <div v-for="book in myBooks" :key="book.id">
-        {{ book.title }} - {{ book.author }}
-      </div>
+      <book
+        :book="book"
+        v-for="book in myBooks"
+        :key="book.id" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue } from 'vue-property-decorator';
+  import { Component, Vue } from 'vue-property-decorator';
   import IBook from '@/types/Book';
   import BookService from '@/services/book-service';
+  import Book from '@/components/Book.vue';
 
   const bookService = new BookService();
 
   @Component({
     name: 'MyBooks',
-    components: {}
+    components: { Book }
   })
   export default class MyBooks extends Vue {
-    // data
     myBooks: IBook[] = [];
 
-    // computed properties
     get bookCount() {
       return this.myBooks.length;
     }
 
-    //props
-
-
-    // methods
-
-
-    // lifecycle hooks
     created() {
       bookService.getAllBooks()
         .then(res => this.myBooks = res)
