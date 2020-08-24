f0e2e8e8802652c993233093feea72d01640b535
Sean <sean.stamm5@gmail.com>
2020-08-24 16:09:15 -0700

    Add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/gb-logo.png">
     <HelloWorld msg="Welcome to GoodBooks"/>
   </div>
 </template>
