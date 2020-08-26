8160d1e2e43582e1d4f7bda8ecc6df71cadcbbda
Sean <sean.stamm5@gmail.com>
2020-08-25 22:22:06 -0700

    Make properties optional for IBook

--- src/types/Book.d.ts
+++ src/types/Book.d.ts
@@ -1,7 +1,7 @@
 export default interface IBook {
-    id: number;
-    createdOn: Date;
-    updatedOn: Date;
+    id?: number;
+    createdOn?: Date;
+    updatedOn?: Date;
     title: string;
     author: string;
 }
